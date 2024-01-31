ce483923515ec91eb783e322149d63fdbe741dfc
petevdp <pjvanderpol@gmail.com>
2024-01-31 15:44:43 -0800

    trim landing page

--- src/client/components/LandingPage.tsx
+++ src/client/components/LandingPage.tsx
@@ -59,65 +59,11 @@ export function LandingPage() {
             <Heading styleLevel={1} className={css({ textAlign: 'center' })}>
               Take notes in real time.
             </Heading>
-            <Paragraph1 as="p">This is another blurb to fill in later. It should be like two sentences.</Paragraph1>
             <span className={css({ display: 'flex', alignItems: 'center' })}>
-              <Label1 as="label" className={css({ marginBottom: '4px', marginRight: '5px' })}>
-                Login with Github to start
-              </Label1>
-              <Button onClick={() => loginWithGithub()}>Log In</Button>
+              <Button onClick={() => loginWithGithub()}>Login With Github</Button>
             </span>
           </span>
-          <img
-            className={css({
-              height: '600px',
-              // width: '1000px',
-              // width: '90vw',
-              padding: '7px',
-              backgroundColor: theme.colors.backgroundAlt,
-              marginLeft: 'auto',
-              marginRight: 'auto',
-            })}
-            src={LightScreenshot1}
-          />
         </div>
-        <nav
-          className={css({
-            listStyle: 'none',
-            display: 'grid',
-            gridTemplateColumns: 'repeat(4, 1fr)',
-            justifyItems: 'center',
-            gridGap: '1rem',
-            marginTop: '1rem',
-            marginBottom: '1rem',
-          })}
-        >
-          <FeatureListItem href="#yjs" Icon={SvgLogo} label="Real-time editing"></FeatureListItem>
-          <FeatureListItem href="#markdown-preview" label="Markdown Previews" Icon={SvgMarkdown}></FeatureListItem>
-          <FeatureListItem href="#github-integrations" Icon={SvgGithub} label="Github Integrations"></FeatureListItem>
-          <FeatureListItem href="#monaco-editor" Icon={SvgVsCode} label="Monaco Editor"></FeatureListItem>
-        </nav>
-        <Heading className={css({ textAlign: 'center' })} styleLevel={2}>
-          Features
-        </Heading>
-        <HeadingLevel>
-          <FeatureSection imageSide="left" id="yjs" label="Real time editing powered by YJS">
-            Edit the same file at the same file with your collaborators using{' '}
-            <a href="https://github.com/yjs/yjs">YJS</a>. YJS is a very fast CRDT implementation which allows Share
-            Notes to maintain low latency and jumps during editing.
-          </FeatureSection>
-          <FeatureSection imageSide="right" id="markdown-preview" label="Markdown Preview">
-            Real time markdown previews using <a href="https://marked.js.org">Marked</a>
-          </FeatureSection>
-          <FeatureSection imageSide="left" id="github-integrations" label="Github Integrations">
-            Easily save your notes to a <a href="https://gist.github.com/">github gist</a>, or load an existing gist
-            into share notes.
-          </FeatureSection>
-          <FeatureSection imageSide="right" id="monaco-editor" label="Monaco Editor">
-            {
-              "Takes advantage of microsoft's Monaco editor; an embeddable extract of the popular Visual Studio Code editor."
-            }
-          </FeatureSection>
-        </HeadingLevel>
       </HeadingLevel>
     </article>
   );
